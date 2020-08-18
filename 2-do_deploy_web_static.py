8f5b0dc8b3e0590b531ecae341b87f1d8d4c0e2d
Andmontc <oscarandresmontes@gmail.com>
2020-08-18 04:32:47 +0000

    script that distributes an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3
 # script that distributes an archive to your web servers
 from fabric.api import *
-import os
+import os 
 
 env.hosts = ['34.75.200.174', '34.229.85.234']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to the
-        web server """
+    """ distributes an archive to web servers """
+    status = True
     if os.path.exists(archive_path) is False:
         return False
-    try:
-        arc = archive_path.split('/')[-1]
-        path = '/data/web_static/releases'
-        directory = arc.split('.')
-        put("{}".format(archive_path), "/tmp/{}".format(arc))
-        run("sudo mkdir -p {}/{}/".format(path, directory[0]))
-        run("sudo tar -xzf /tmp/{} -C {}/{}/".format(arc, path, directory[0]))
-        run("sudo rm /tmp/{}".format(arc))
-        run("sudo mv {}/{}/web_static/* {}/{}/\
-            ".format(path, directory[0], path, directory[0]))
-        run("sudo rm -rf {}/{}/web_static".format(path, directory[0]))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {}/{}/ /data/web_static/current\
-            ".format(path, directory[0]))
-        print('New version deployed!')
-        return True
-    except Exception:
-        return False
+    # Upload the file
+    if put('{}'.format(archive_path), '/tmp/').failed is True:
+        status = False
+    file_local = archive_path.split('/')[-1]
+    file_server = file_local.split('.')[0]
+    # Create the folder
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
+           format(file_server)).failed is True:
+        status = False
+    # Decompress the file into server
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file_local, file_server)).failed is True:
+        status = False
+    # Delete the file from tmp
+    if run("sudo rm -rf /tmp/{}".format(file_local)).failed is True:
+        status = False
+    # Move the file
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".
+           format(file_server, file_server)).failed is True:
+        status = False
+    # Delete the archive from the web server
+    if run("sudo rm -rf /data/web_static/releases/{}/".
+           format(file_server)).failed is True:
+        status = False
+    # Delete the symbolic link
+    if run("sudo rm -rf /data/web_static/current").failed is True:
+        status = False
+    # Delete the symbolic link
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_server)).failed is True:
+        status = False
+    return status
