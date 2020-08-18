83ecbab06c6f7332560510efb6a6e24817ab1a43
Andmontc <oscarandresmontes@gmail.com>
2020-08-18 15:52:49 +0000

    Script that delete out of date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,15 +8,15 @@ env.hosts = ['34.75.200.174', '34.229.85.234']
 
 def do_clean(number=0):
     """Delete out-of-date archives """
-    num = 1 if int(num) == 0 else int(num)
+    number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(num)]
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(num)]
+        [archives.pop() for i in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
