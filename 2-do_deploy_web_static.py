a8c7e34d1417c07e1c2de5638ac2e614416c35e0
Andmontc <oscarandresmontes@gmail.com>
2020-08-18 03:03:51 +0000

    distributes an archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,38 +8,39 @@ env.hosts = ['34.75.200.174', '34.229.85.234']
 
 def do_deploy(archive_path):
     """ distributes an archive to web servers """
+    status = True
     if not path.exists(archive_path):
         return False
     file_local = archive_path.split('/')[-1]
     file_server = file_local.split('.')[0]
     # Upload the file
     if put(archive_path, "/tmp/{}".format(file_local)).failed is True:
-        return False
+        status = False
     # Create the folder
     if run("mkdir -p /data/web_static/releases/{}/".
            format(file_server)).failed is True:
-        return False
+        status = False
     # Decompress the file into server
     if run("tar -xzf/tmp/{} -C /data/web_static/releases/{}/".
            format(file_local, file_server)).failed is True:
-        return False
+        status = False
     # Delete the file from tmp
     if run("rm /tmp/{}".format(file_local)).failed is True:
-        return False
+        status = False
     # Move the file
     if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".
            format(file_server, file_server)).failed is True:
-        return False
+        status = False
     # Delete the archive from the web server
     if run("rm -rf /data/web_static/releases/{}/".
            format(file_server)).failed is True:
-        return False
+        status = False
     # Delete the symbolic link
     if run("rm -rf /data/web_static/current").failed is True:
-        return False
+        status = False
     # Delete the symbolic link
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(file_server)).failed is True:
-        return False
-    return True
+        status = False
+    return status
