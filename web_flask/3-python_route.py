0f010da35ef70e409f1f6fe9706a976d88b75bad
Andmontc <oscarandresmontes@gmail.com>
2020-08-31 20:22:23 +0000

    Python Route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Flask module """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function that display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display on route hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ display C and value text """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ display Python and value text """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
