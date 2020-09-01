d1a2d0e7f3434f47a9f72203013fb2f497a27181
Andmontc <oscarandresmontes@gmail.com>
2020-09-01 17:20:43 +0000

    C route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Flask module """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function that display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display on route hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ display C and value text """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
