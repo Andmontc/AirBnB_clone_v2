c0ebeb3080f4a933f8eaeed7551d24596651225b
Andmontc <oscarandresmontes@gmail.com>
2020-09-02 00:21:20 +0000

    Display state and cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Flask module """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_states_cities():
+    """ function that display a page with the city of the states """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Close Sql session """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
